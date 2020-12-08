2c4f72935af19e20ea93f0cf208ebc3d2b0b6e79
Diwanga <diwangaamasith@gmai.com>
2020-12-08 23:57:33 +0530

    send 1st response

--- 2 first server/app.js
+++ 2 first server/app.js
@@ -4,6 +4,20 @@ const http = require('http');
 const server = http.createServer((req, res) => {
 
     console.log(req);
+    res.setHeader("Content-type", "text/html");
+    res.write('<html>');
+    res.write('<head><title>MY first node response</title></head>');
+
+
+
+    res.write('<body><h1> DIWANGA AMASITH KARIYWASAM</h1></body>');
+    res.write('</head>');
+    res.write('</head>');
+
+
+    res.write('</html>');
+    res.end();
+
 
 });
 
