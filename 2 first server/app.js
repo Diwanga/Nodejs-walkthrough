4e0c860068945c5dc46b382ac7ddac6532f690dd
Diwanga <diwangaamasith@gmai.com>
2020-12-09 12:09:15 +0530

    first routing

--- 2 first server/app.js
+++ 2 first server/app.js
@@ -3,20 +3,25 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
-    console.log(req);
+    // console.log(req);
+    const url = req.url
+    if (url === "/") {
+        res.setHeader("Content-type", "text/html");
+        res.write('<html>');
+        res.write('<head><title>MY first node response</title></head>');
+        res.write('<body> <form action ="/messaage" method ="POST"><input type="text" name = "message"> <button type="submit">SEND</button></form></body>');
+        res.write('</head>');
+        res.write('</head>');
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader("Content-type", "text/html");
     res.write('<html>');
-    res.write('<head><title>MY first node response</title></head>');
-
-
-
+    res.write('<head><title>node req routing</title></head>');
     res.write('<body><h1> DIWANGA AMASITH KARIYWASAM</h1></body>');
     res.write('</head>');
     res.write('</head>');
-
-
     res.write('</html>');
-    res.end();
 
 
 });
